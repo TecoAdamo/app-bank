ef630df7135e5e4bd15115ca77059b7b4c32f417
Mateus Adamo <adamomateus478@gmail.com>
2024-01-31 23:44:49 -0300

    feat: Input of name and age created.

--- src/Screens/Home.tsx
+++ src/Screens/Home.tsx
@@ -1,7 +1,46 @@
-import InputName from '../Components/InputName';
+import * as React from 'react';
+
+import { StyleSheet, View } from 'react-native';
+
+import { TextInput } from 'react-native-paper';
 
 export default function Home() {
+
     return (
-        <InputName />
+        <View style={styles.container}>
+
+            <View style={styles.boxInput}>
+                <View>
+                    <TextInput
+                        mode="outlined"
+                        label="Insira seu nome:"
+                    />
+                </View>
+                <View style={styles.boxInputAge}>
+                    <TextInput
+                        mode="outlined"
+                        label="Insira sua idade:"
+                        keyboardType='numeric'
+                    />
+                </View>
+
+            </View>
+
+        </View>
     );
-}
+};
+
+const styles = StyleSheet.create({
+    container: {
+        alignItems: 'center'
+    },
+    boxInput: {
+        top: '20%',
+        width: '90%',
+        height: '100%',
+        bottom: '26%',
+    },
+    boxInputAge: {
+        marginTop: '4%'
+    }
+});
